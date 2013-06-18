eb41ed37dff84322c08abdc455d6449465b95ce0
Stephen Simpson <me@simpo.org>
2013-06-18 07:33:27 +0100

    Added extra delete code to get rid of extra files created by the closure compiler.

--- scripts/build.bat
+++ scripts/build.bat
@@ -1,4 +1,7 @@
 cls
 node dojo/dojo.js load=build --profile pin.profile.js --release
+del release\*.js.uncompressed.js /S /Q
+del release\*.js.map /S /Q
+del release\*.js.consoleStripped.js /S /Q
 del release\*.uncompressed.js /S /Q
 komodo release\build-report.txt
